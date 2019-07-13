df8430ab78d2797bea6b97499514ffc1da6b7c33
Juan Pablo <juanpablopizarro97@gmail.com>
2019-07-13 17:06:11 -0500

    Se realizaron ajustes y validaciones en el servidor y se crearon los objetos Json correspondientes al esquema models

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -8,19 +8,50 @@ usercontroller.getUsers = async (req, res)=>{
 }
 
 usercontroller.createUser = async (req, res)=>{
+    // creamos un arreglo de errores
+    const errors = [];
+    
     // validamos email repetido
     const emailUser = await User.findOne({email: req.body.email});
     if(emailUser){
-        console.log("Ya existe un usurio con el email") 
+        errors.push({text: "Ya existe un usurio con el email"});
+        console.log("Ya existe un usurio con el email");
         //res.flash('error_msg', 'The Emain is already in use');
-         // rediccionar al signup....
-         //res.redirect('/');
+         // renderizar al Registro....
+         //res.render('/');
     }
-    else
+
+    const {password,confirmPass,name} = req.body;
+    if(password != confirmPass)
+    {
+        console.log("Las contraseñas no coinsiden");
+        errors.push({text: "Las contraseñas no coinsiden"});
+    }
+
+    if(password.length < 4)
+    {
+        console.log("Las contraseña debe ser de alemnos 4 caracteres");
+        errors.push({text: "Las contraseñas no coinsiden"});
+    }
+
+    if(name=""){
+        console.log("Por favor ingrese su nombre de usuario");
+        errors.push({text: "Por favor ingrese su nombre de usuario"});
+    }
+    
+    //Usamos una expresion regular para validar que el campo del nombre no permita caracteres especiales
+    const valCarEsp = new RegExp('^[A-Z]+$', 'i')
+    if(!valCarEsp.test(name))
+    {
+        console.log("El nombre de usuario debe no permite caracteres especiales");
+        errors.push({text: "El nombre de usuario debe no permite caracteres especiales"});
+    }
+    
+    if(errors.length = 0)
     {
         const user = new User(req.body);
         // encriptamos la contraseña
-        //user.password = await user.encryptPassword(password);
+        user.password = await user.encryptPassword(req.body.password);
         await user.save();
         console.log(user) // en la propiedad body tomamos los datos que nos esta enviando el cliente
    
@@ -29,6 +60,11 @@ usercontroller.createUser = async (req, res)=>{
         });
 
         // redireccionar al login....
+    }else
+    {
+
+        // rendrizar al Registro....
+         //res.render('/',{errors, emailUser, password, confirmPass, name});
     }
 
     
@@ -36,7 +72,14 @@ usercontroller.createUser = async (req, res)=>{
 
 usercontroller.getUser = async (req, res)=>{
     const user = await User.findById(req.params.id);
-    res.json(user);
+    if(user){
+        res.json(user);
+    }
+    else
+    {
+        res.json({status : "El usuario no existe"})
+    }
+    
 };
 
 usercontroller.editUser = async (req, res)=>{
--- server/models/users.js
+++ server/models/users.js
@@ -16,16 +16,16 @@ const  userSchema = new Schema({
 });
 
 // cifrando contraseña
-// userSchema.methods.encryptPassword = async (password)=>{
-//     const salt = await bcrypt.genSalt(10); // realizar el algoritmo 10 veces
-//     const hash = bcrypt.hash(password,salt); // generamos la contraseña cifrada con el hash creado anteriormente
-//     return hash;
-// };
+userSchema.methods.encryptPassword = async (password)=>{
+    const salt = await bcrypt.genSalt(10); // realizar el algoritmo 10 veces
+    const hash = bcrypt.hash(password,salt); // generamos la contraseña cifrada con el hash creado anteriormente
+    return hash;
+};
 
-// //Comparamos la contraseña que nos envia el usuario con la que hay en la base de datos
+//Comparamos la contraseña que nos envia el usuario con la que hay en la base de datos
 // //usamos ES5 para poder usar el metodo this
-// userSchema.method.matchPassword = async function (password){
-//     return bcrypt.compare(password,this.password);
-// }
+userSchema.method.matchPassword = async function (password){
+    return bcrypt.compare(password,this.password);
+}
 
 module.exports = mongoose.model('User',userSchema);
